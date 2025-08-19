f6f1ff75c59fa97acf9f859733715d9575090b23
hoangnguyen2003 <ninjadoc0152@gmail.com>
2025-08-20 02:14:41 +0700

    revert

--- dataset.py
+++ dataset.py
@@ -101,27 +101,6 @@ def MMDataLoader(args):
         'test': MMDataset(args, mode='test')
     }
 
-    print('\n\n\n')
-    print("\n\n=== Dataset Info ===")
-    for split in ['train', 'valid', 'test']:
-        dataset = datasets[split]
-        print(f"\n--- {split} ---")
-        print(f"Number of samples: {len(dataset)}")
-
-        # Lấy thử một sample để xem shape
-        sample = dataset[0]
-        for key, value in sample.items():
-            if isinstance(value, torch.Tensor):
-                print(key, value.shape)
-            elif isinstance(value, dict):  # labels
-                for k, v in value.items():
-                    print(f"labels[{k}]", v.shape)
-            else:
-                print(key, type(value))
-
-    print("\n\n")
-    exit()
-
     if 'seq_lens' in args:
         args.seq_lens = datasets['train'].get_seq_len() 
 
